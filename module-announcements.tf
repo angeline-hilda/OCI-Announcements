4627d0d85d1627dbafe1cbd2696ddf4e06a87cb2
angeline-hilda <angelinehilda97@gmail.com>
2025-03-21 17:27:04 +0530

    Updated file with new changes

--- module-announcements.tf
+++ module-announcements.tf
@@ -1,23 +1,23 @@
 module "announcements" {
-    source = "./announcements"
+  source = "./announcements"
 
-    compartment_id = var.compartment_id
-    topic_id = var.topic_id
+  compartment_id = var.compartment_id
+  topic_id       = var.topic_id
 
-    announcement_subscription_display_name = var.announcement_subscription_display_name
-    announcement_subscription_description = var.announcement_subscription_description
+  announcement_subscription_display_name = var.announcement_subscription_display_name
+  announcement_subscription_description  = var.announcement_subscription_description
 
-    announcement_subscription_preferred_language =   var.announcement_subscription_preferred_language
-    announcement_subscription_preferred_time_zone =  var.announcement_subscription_preferred_time_zone
+  announcement_subscription_preferred_language  = var.announcement_subscription_preferred_language
+  announcement_subscription_preferred_time_zone = var.announcement_subscription_preferred_time_zone
 
-    use_filter = var.use_filter
-    filter_groups = var.filter_groups
+  use_filter    = var.use_filter
+  filter_groups = var.filter_groups
 
-       
 
-    providers = {
+
+  providers = {
     oci             = oci
     oci.home_region = oci.home_region
   }
-  
+
 }
